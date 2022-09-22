5ecaca07737b2b98ac4e0d3b4f5257d7e20290ea
ESOCODES <ejirosamuelolori@gmail.com>
2022-09-21 19:35:01 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - An entry point
+ *
+ *   *
+ *
+ *    * Description: "Programming is like building a multilingual puzzle"
+ *
+ *     * Return: 0 if exited and non_zero if otherwise
+ *
+ *      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
