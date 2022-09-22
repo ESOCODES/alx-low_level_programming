2f26cb8cb1077a21f1b56ed0b36537929e4860c8
ESOCODES <ejirosamuelolori@gmail.com>
2022-09-22 15:33:46 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate 2 strings
+ * @dest: input
+ * @src: input
+ * @n: integer
+ *
+ * Return: temp
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+	while (*dest)
+		dest++;
+	if (n > srclen)
+		n = srclen;
+	src = start;
+	for (; i < n; i++)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (temp);
+}
